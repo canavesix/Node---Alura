743892e04c53f2bb24df138ffd117aaecad27166
canavesix <gustavo.canavesi@outlook.com>
2025-08-26 19:08:39 -0300

    Subindo o restante dos exercicios de JSON, finalizando módulo

--- manipulacaoJson.js
+++ manipulacaoJson.js
@@ -1,23 +1,59 @@
 
 const fs = require('fs');
-// const animaisString = JSON.stringify(animais)
+const animais = require('./animais.json')
+
+const animaisString = JSON.stringify(animais)
 // console.log(animaisString);
 
-// const animaisObject = JSON.parse(animaisString)
+const animaisObject = JSON.parse(animaisString)
 // console.log(animaisObject)
 
 
+// Adicionando informações no JSON
+// const novoAnimal = {id: 4, nome: "Gavião", tipo: "Ave", habitat: "Floresta"};
 
-const novoAnimal = {
-    "id": 4,
-    "nome": "Gavião",
-    "tipo": "Ave",
-    "habitat": "Floresta"
+// animais.animais.push(novoAnimal);
+
+
+// fs.writeFileSync('./animais.json', JSON.stringify(animais, null, 2));
+
+// console.log(animais);
+
+
+
+// alterar propriedade de um json
+
+// const animal = animais.animais.find(a => a.id === 4);
+
+// if(animal){
+//     animal.habitat = "Montanha";
+// }
+
+// fs.writeFileSync('./animais.json', JSON.stringify(animais, null, 2));
+
+// console.log(animais);
+
+
+
+// Remover propriedade
+// animais.animais = animais.animais.filter(a => a.id !== 4);
+// fs.writeFileSync('./animais.json', JSON.stringify(animais, null, 2));
+// console.log(animais);
+
+
+// =================================
+
+
+
+
+const pessoalOriginal = {
+    id: 1,
+    nome: "Gustavo",
+    idade: 25
 }
 
-const animais = require('./animais.json');
-// animais.push(novoAnimal);
-const animaisString = JSON.stringify(animais);
-// console.log(animaisString);
+const copiaProfundaPessoa = JSON.parse(JSON.stringify(pessoalOriginal));
 
-console.log(animais)
+copiaProfundaPessoa.nome = "Gustavo Canavesi";
+console.log(copiaProfundaPessoa);
+console.log(pessoalOriginal);
