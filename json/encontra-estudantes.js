82b4428e5ef6a901cfdabc2ff1db7f734987d8aa
canavesix <gustavo.canavesi@outlook.com>
2025-08-27 14:44:39 -0300

    Organizando as pastas e iniciando o projeto: Primeira biblioteca

--- json/encontra-estudantes.js
+++ json/encontra-estudantes.js
@@ -1,4 +1,4 @@
-const estudantes = require('./estudantes.json');
+const estudantes = require('../estudantes.json');
 
 function buscaInformacao(lista, chave, valor) {
     return lista.find((estudante) => estudante[chave].includes(valor));
