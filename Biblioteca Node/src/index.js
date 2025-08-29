475576dfeccd7911d44bd49f0190ae285b88822e
canavesix <gustavo.canavesi@outlook.com>
2025-08-29 08:17:50 -0300

    Capturando palavras e mapeando melhorias

--- Biblioteca Node/src/index.js
+++ Biblioteca Node/src/index.js
@@ -5,9 +5,28 @@ const caminhoArquivo = process.argv;
 const link = caminhoArquivo[2];
 
 fs.readFile(link, 'utf-8', (erro, texto) =>{
-    console.log(texto);
+    verficaPalavrasDuplicadas(texto);
 })
 
 
+// percorrer o texto e verficar palavras duplicadas
+// criar um array de palavras
+// contar as ocorrencias
+// montar um objeto com o resultado:
+
+// {
+//     "web": 5;,
+//     "computador": 4
+// }
 
 
+function verficaPalavrasDuplicadas(texto){
+    const listaPalavra = texto.split(' ');
+    const resultado = {};
+    // objeto[propriedade] = valor;
+    listaPalavra.forEach(palavra => {
+        resultado[palavra] = (resultado[palavra] || 0) + 1
+    })
+    console.log(resultado);
+    
+}
