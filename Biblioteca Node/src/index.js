f323dc48dca78a3760e7c8bf12e33c35c1fdea69
Gustavo Canavesi <gustavo.canavesi@outlook.com>
2025-09-03 08:44:20 -0300

    Cercando erros e utilizando try catch

--- Biblioteca Node/src/index.js
+++ Biblioteca Node/src/index.js
@@ -10,12 +10,13 @@ const caminhoArquivo = process.argv;
 const link = caminhoArquivo[2];
 
 fs.readFile(link, 'utf-8', (erro, texto) =>{
-    if(erro){
-        console.log('Qual é o erro?', erro);
-        return
+    try {
+      if (erro) throw erro
+      contaPalavras(texto);
+    } catch(erro){
+      if(erro.code === 'ENOENT') console.log('Erro esperado');
+      else console.log('Outro erro') 
     }
-    console.log('Qual é o erro?', erro);
-    contaPalavras(texto);
 })
 
 function contaPalavras(texto){
